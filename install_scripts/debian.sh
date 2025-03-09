294739651c48e5678726f26d69dd47dee8410723
gurudath350 <82510523+gurudath350@users.noreply.github.com>
2025-03-09 14:11:44 +0530

    Update debian.sh

--- install_scripts/debian.sh
+++ install_scripts/debian.sh
@@ -4,8 +4,14 @@ TOOL=$1
 case $TOOL in
     "docker")
         sudo apt install -y docker.io ;;
+    "kubectl")
+        sudo snap install kubectl --classic ;;
     "terraform")
         sudo apt install -y terraform ;;
+    "java")
+        sudo apt install -y openjdk-17-jdk ;;
+    "nodejs")
+        sudo apt install -y nodejs npm ;;
     *)
         echo "Tool not supported" ;;
 esac
